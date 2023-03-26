5970d64957d69b906610085fc8ccb6ca4cc36b22
Cosmin Ene <nationalsown@gmail.com>
2023-03-26 12:45:04 -0400

    Created factories for products - unfinished

--- SoftDesPhase2Engine/Classes/ProductFactoryAbstract.cs
+++ SoftDesPhase2Engine/Classes/ProductFactoryAbstract.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SoftDesPhase2Engine.Classes
 {
-    public class ProductFactory
+    public abstract class ProductFactoryAbstract
     {
-    }
+        public abstract Product createProduct();
+ 
 }
---
+++ SoftDesPhase2Engine/Classes/ProductFactoryConcrete.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftDesPhase2Engine.Classes
+{
+    public class ProductFactoryConcrete : ProductFactoryAbstract
+    {
+        
+
+        public override Product createProduct()
+        {
+
+            
+            List<Type> types = new List<Type>();
+            types.Add(typeof(Carrot));
+            types.Add(typeof(Cauliflower));
+            types.Add(typeof(Peas));
+            types.Add(typeof(Potato));
+            types.Add(typeof(Seeds));
+
+            Random random = new Random();
+
+            int next = random.Next(types.Count);
+
+            object item;
+            item = types[next].MakeGenericType();
+
+            return (Product)item;
+        }
+    }
+}
